d908d8b758e96613cfdeff58ed2e06f5b45ff9a3
Benjamin Holmes <benjamin.holmes@gmail.com>
2022-08-18 20:06:02 +0100

    fixed output format

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "private_ip" {
-    value       = module.rhel.rhel_private_ip
+    value       = module.rhel.*.rhel_private_ip
     description = "The Private IP Adress of the Azure Virtual Machine Instance"
 }
 
 output "public_ip" {
-    value       = module.rhel.rhel_public_ip
+    value       = module.rhel.*.rhel_public_ip
     description = "The Public IP Adress of the Azure Virtual Machine Instance"
 }
 
 output "default_username" {
-    value       = module.rhel.rhel_default_username
+    value       = module.rhel.*.rhel_default_username
     description = "The Default Admin Username of the Azure Virtual Machine Instance"
 }
