11649812012b05b6bb8814793b815d4ac540b612
Benjamin Holmes <benjaminholmes@benjaminholmes-hmx933964v.orbital.local>
2023-01-28 00:03:46 +0000

    updated module version

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ provider "random" {
 
 module "rhel" {
   source  = "app.terraform.io/ben-holmes/rhel-standard/azurerm"
-  version = "0.0.2"
+  version = "0.0.4"
   count   = var.vm_instance_count
 
   vm_name_prefix            = var.vm_name_prefix
-  vm_instance_count         = count.index
+  vm_count_suffix           = count.index
   vm_owner                  = var.vm_owner
   rg_name                   = var.rg_name
   ssh_admin_user_public_key = var.ssh_admin_user_public_key
