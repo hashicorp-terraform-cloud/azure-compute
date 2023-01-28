86028c702c7bf43545c61d85cb4d1c465a828c1d
Benjamin Holmes <benjaminholmes@benjaminholmes-hmx933964v.orbital.local>
2023-01-28 00:30:45 +0000

    updates following module refactoring

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "rhel_vm_id" {
-  value       = module.rhel.*.rhel_vm_id
+  value       = module.rhel-standard.*.rhel_vm_id
   description = "The ID of the Azure Virtual Machine Instance"
 }
 
 output "private_ip" {
-  value       = module.rhel.*.rhel_private_ip
+  value       = module.rhel-standard.*.rhel_private_ip
   description = "The Private IP Adress of the Azure Virtual Machine Instance"
 }
 
 output "public_ip" {
-  value       = module.rhel.*.rhel_public_ip
+  value       = module.rhel-standard.*.rhel_public_ip
   description = "The Public IP Adress of the Azure Virtual Machine Instance"
 }
 
 output "default_username" {
-  value       = module.rhel.*.rhel_default_username
+  value       = module.rhel-standard.*.rhel_default_username
   description = "The Default Admin Username of the Azure Virtual Machine Instance"
 }
