673f5ee021993d84cef67c1389a1f7b5a84014d5
Ben Holmes <ben.holmes@hashicorp.com>
2023-03-06 12:31:47 +0000

    bumped module version

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ locals {
 
 module "rhel-standard" {
   source  = "app.terraform.io/ben-holmes/rhel-standard/azurerm"
-  version = "0.0.5"
+  version = "0.0.7"
   count   = var.vm_instance_count
 
   vm_name                   = "${local.vm_name}-${count.index}"
